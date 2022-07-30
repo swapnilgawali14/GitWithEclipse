d612fea6d553a83887a01cc5d288e1b47584c9f7
Swapnil Gawali <swapnilgawali1114@gmail.com>
2022-07-30 14:09:09 +0530

    commit from branch1

--- testingProject1/src/testingProject1/GitFromCollaborator.java
+++ testingProject1/src/testingProject1/GitFromCollaborator.java
@@ -4,7 +4,7 @@ public class GitFromCollaborator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("this is collaborator");
+		System.out.println("this is collaboratorrrr");
 
 	}
 
