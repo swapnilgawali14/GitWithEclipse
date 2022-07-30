afd566d312551e01cc7a48ae7fa07c1e89024a8c
Swapnil Gawali <swapnilgawali1114@gmail.com>
2022-07-30 12:53:37 +0530

    2nd commit

--- testingProject1/src/testingProject1/OpenUrl.java
+++ testingProject1/src/testingProject1/OpenUrl.java
@@ -1,14 +1,16 @@
 package testingProject1;
 
+import org.openqa.selenium.WebDriver;
+
 public class OpenUrl {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args){
 		// TODO Auto-generated method stub
 
 		//setting property of chrome and passing the chrome driver path  
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Lenovo\\eclipse-workspace\\chromedriver.exe");
 		//Creating and launching instance of chrome driver 
-		WebDriver driver = new ChromeDriver();
+		WebDriver driver = new chrome);
 		
 	}
 
